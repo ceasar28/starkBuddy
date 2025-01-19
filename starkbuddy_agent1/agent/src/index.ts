2edfa17394047a8e3418a7be2cd01c50f8564717
ceasar28 <emmanuelugochukwuekete@gmail.com>
2025-01-19 11:42:16 +0100

    starkbuddy character

--- starkbuddy_agent1/agent/src/index.ts
+++ starkbuddy_agent1/agent/src/index.ts
@@ -45,7 +45,7 @@ import { fileURLToPath } from "url";
 import yargs from "yargs";
 // import { mainCharacter } from "../mainCharacter.ts";
 // import { mainCharacter } from "../skynetCharacter.ts";
-import { mainCharacter } from "../TRUTHCharacter.ts";
+import { mainCharacter } from "../STARKBUDDY_CHARACTER.ts";
 
 const __filename = fileURLToPath(import.meta.url); // get the resolved path to the file
 const __dirname = path.dirname(__filename); // get the name of the directory
