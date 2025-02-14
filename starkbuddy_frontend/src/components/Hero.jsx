45360f02a3d2bb8f64ad0355e022a065190f9fc9
Adaobi-Chuks <chuksaginamada@gmail.com>
2025-02-14 23:17:08 +0100

    Feat: Final update frontend

--- starkbuddy_frontend/src/components/Hero.jsx
+++ starkbuddy_frontend/src/components/Hero.jsx
@@ -5,9 +5,9 @@ const Hero = () => {
         <section className="flex flex-col items-center justify-center text-center px-4 py-8 min-h-[70vh] md:min-h-[40vh]">
             <h1 className="text-4xl md:text-6xl font-bold text-[#3fe8ab] mb-6">StarkBuddy</h1>
             <p className="text-xs md:text-lg max-w-2xl text-white leading-relaxed">
-                Your AI-powered assistant for actionable insights on the StarkNet blockchain. <br />
-                Chat with StarkBuddy, interact with our Twitter and Telegram agents. <br />
-                Track transactions, analyze tokens, and stay informed with real-time updates.
+                Your AI-powered assistant for deep insights into the StarkNet blockchain. <br />
+                Chat with StarkBuddy, explore transactions, analyze token sentiment, and track market trends in real time. <br />
+                Engage with our AI agents on Twitter and Telegram for seamless interaction.
             </p>
         </section>
     );
